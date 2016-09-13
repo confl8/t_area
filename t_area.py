eee341a36381d77a8f96b466e17edba2053c14df
Gerald Erickson <geraldraymonderickson@gmail.com>
2016-09-13 19:11:15 -0400

    Update t_area.py

--- t_area.py
+++ t_area.py
@@ -1,25 +1,14 @@
-# Introduce yourself
-name = input('Enter your name: ')
-print('Hello', name)
-
 # Define our variables, starting with the base measurement
 
-x = float(input("Please enter the numerical value of the base of the triangle:"))
-
-# Make sure a negative number isn't given
-
-if x < 0:
-    print('base cannot be less than 0')
+x = float(input("Please enter the numerical value for the base of the triangle:"))
 
 # Same thing, but now the height
     
-y = float(input("Please enter the numerical value of the height of the triangle:"))
-
-if y < 0:
-    print('height cannot be less than 0')
+y = float(input("Please enter the numerical value for the height of the triangle:"))
 
 # Now have the previous notes equal our area
 
-a = (x * y )* 0.5
+area = (x * y )* 0.5
 
-print('The area of the triangle is' ,a)
+# The calculation for area completed
+print('The area of the triangle is', area)
